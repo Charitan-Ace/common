a7ebb40153dcdf28bb5b7024e036d22e4a3252bb
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-27 21:31:47 +0700

    get country dto

---
+++ src/main/java/ace/charitan/common/dto/country/GetCountryByIsoCode.java
@@ -0,0 +1,41 @@
+package ace.charitan.common.dto.country;
+
+import ace.charitan.common.dto.DtoWithProcessId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class GetCountryByIsoCode {
+    public static class GetCountryByIsoCodeRequestDto extends DtoWithProcessId {
+
+        private String isoCode;
+
+        public GetCountryByIsoCodeRequestDto() {
+
+        }
+
+        public GetCountryByIsoCodeRequestDto(String correlationId, String isoCode) {
+            super(correlationId);
+            this.isoCode = isoCode;
+        }
+
+        public String getIsoCode() {
+            return isoCode;
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class GetCountryByIsoCodeResponseDto extends DtoWithProcessId {
+
+        private Long id;
+
+        private String isoCode;
+
+        private String regionName;
+    }
+
+}
