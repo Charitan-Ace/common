d570914c5da859758532df8706d023768c47e0f9
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-30 19:24:46 +0700

    add to v4

--- src/main/java/ace/charitan/common/dto/country/GetCountryByIsoCode.java
+++ src/main/java/ace/charitan/common/dto/country/GetCountryByIsoCode.java
@@ -1,35 +1,26 @@
 package ace.charitan.common.dto.country;
 
-import ace.charitan.common.dto.DtoWithProcessId;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class GetCountryByIsoCode {
-    public static class GetCountryByIsoCodeRequestDto extends DtoWithProcessId {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetCountryByIsoCodeRequestDto {
 
         private String isoCode;
 
-        public GetCountryByIsoCodeRequestDto() {
-
-        }
-
-        public GetCountryByIsoCodeRequestDto(String correlationId, String isoCode) {
-            super(correlationId);
-            this.isoCode = isoCode;
-        }
-
-        public String getIsoCode() {
-            return isoCode;
-        }
     }
 
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    public class GetCountryByIsoCodeResponseDto extends DtoWithProcessId {
+    public class GetCountryByIsoCodeResponseDto {
 
         private Long id;
 
