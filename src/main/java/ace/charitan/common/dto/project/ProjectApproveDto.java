af701b3c976a86dda528342f2ac27c11039ded79
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-02 23:03:30 +0700

    feat: project ID as string

--- src/main/java/ace/charitan/common/dto/project/ProjectApproveDto.java
+++ src/main/java/ace/charitan/common/dto/project/ProjectApproveDto.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectApproveDto {
-    private Long id;
+  private String id;
 }
--- src/main/java/ace/charitan/common/dto/project/ProjectHaltDto.java
+++ src/main/java/ace/charitan/common/dto/project/ProjectHaltDto.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectHaltDto {
-    private Long id;
+  private String id;
 }
