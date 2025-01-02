9640c60ad6ba83a43c9dcf03067b2cd6b9c9fa5c
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-02 21:02:12 +0700

    feat: add project management dto

---
+++ src/main/java/ace/charitan/common/dto/project/ProjectApproveDto.java
@@ -0,0 +1,12 @@
+package ace.charitan.common.dto.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectApproveDto {
+    private Long id;
+}
---
+++ src/main/java/ace/charitan/common/dto/project/ProjectHaltDto.java
@@ -0,0 +1,12 @@
+package ace.charitan.common.dto.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectHaltDto {
+    private Long id;
+}
