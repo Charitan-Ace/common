df66922e2b6204494e24979d8f0f522364cb887c
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-08 21:09:00 +0700

    feat: add project halt and approve email DTOs

---
+++ src/main/java/ace/charitan/common/dto/email/project/EmailProjectHaltCharityDto.java
@@ -0,0 +1,3 @@
+package ace.charitan.common.dto.email.project;
+
+public record EmailProjectHaltCharityDto(String recipient, String subject, String body) {}
---
+++ src/main/java/ace/charitan/common/dto/email/project/EmailProjectHaltDonorDto.java
@@ -0,0 +1,3 @@
+package ace.charitan.common.dto.email.project;
+
+public record EmailProjectHaltDonorDto(String recipient, String subject, String body) {}
