144da3c8d46e2206bd7b319bf252c97abc2ab380
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-10 10:45:48 +0700

    add new project to subscription dto

---
+++ src/main/java/ace/charitan/common/dto/project/ExternalProjectDto.java
@@ -0,0 +1,34 @@
+package ace.charitan.common.dto.project;
+
+import java.time.ZonedDateTime;
+
+import ace.charitan.common.dto.project.ProjectEnum.CategoryType;
+import ace.charitan.common.dto.project.ProjectEnum.StatusType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternalProjectDto {
+
+    private String title;
+
+    private String description;
+
+    private Double goal;
+
+    private ZonedDateTime startTime;
+
+    private ZonedDateTime endTime;
+
+    private StatusType statusType;
+
+    private CategoryType categoryType;
+
+    private String countryIsoCode;
+
+    private String charityId;
+
+}
