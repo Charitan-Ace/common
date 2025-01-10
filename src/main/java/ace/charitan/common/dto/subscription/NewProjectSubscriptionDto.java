c82d8bc98aeb9afde57821eeb78534222564cfb4
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-10 10:52:07 +0700

    modify project external

--- src/main/java/ace/charitan/common/dto/subscription/NewProjectSubscriptionDto.java
+++ src/main/java/ace/charitan/common/dto/subscription/NewProjectSubscriptionDto.java
@@ -1,12 +1,14 @@
 package ace.charitan.common.dto.subscription;
 
+import java.io.Serializable;
+
 import ace.charitan.common.dto.project.ExternalProjectDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-public class NewProjectSubscriptionDto {
+public class NewProjectSubscriptionDto implements Serializable {
 
     @Getter
     @NoArgsConstructor
