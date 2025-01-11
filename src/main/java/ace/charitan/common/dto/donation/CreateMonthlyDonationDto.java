532372807f27c2534da3eb39f7ae8dd300654c74
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-11 14:58:18 +0700

    Added monthly donation dto

---
+++ src/main/java/ace/charitan/common/dto/donation/CreateMonthlyDonationDto.java
@@ -0,0 +1,9 @@
+package ace.charitan.common.dto.donation;
+
+public class CreateMonthlyDonationDto {
+    private Float amount;
+    private String message;
+    private String transactionStripeId;
+    private String projectId;
+    private String donorId;
+}
