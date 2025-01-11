639e121c1916bc5452233a2fa1cfadbb825f3677
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-11 15:11:59 +0700

    added getters

--- src/main/java/ace/charitan/common/dto/donation/CreateMonthlyDonationDto.java
+++ src/main/java/ace/charitan/common/dto/donation/CreateMonthlyDonationDto.java
@@ -1,5 +1,12 @@
 package ace.charitan.common.dto.donation;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateMonthlyDonationDto {
     private Float amount;
     private String message;
