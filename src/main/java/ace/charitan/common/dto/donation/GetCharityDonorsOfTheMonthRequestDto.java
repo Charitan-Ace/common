d2c54b5d6b519a6147b54dda017ee682161e744a
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-12 01:23:37 +0700

    Update dotm dtos

--- src/main/java/ace/charitan/common/dto/donation/GetCharityDonorsOfTheMonthRequestDto.java
+++ src/main/java/ace/charitan/common/dto/donation/GetCharityDonorsOfTheMonthRequestDto.java
@@ -1,4 +1,12 @@
 package ace.charitan.common.dto.donation;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetCharityDonorsOfTheMonthRequestDto {
+    private GetCharityDonorsOfTheMonthWrapperDto wrapper;
 }
--- src/main/java/ace/charitan/common/dto/donation/GetCharityDonorsOfTheMonthWrapperDto.java
+++ src/main/java/ace/charitan/common/dto/donation/GetCharityDonorsOfTheMonthWrapperDto.java
@@ -1,8 +1,15 @@
 package ace.charitan.common.dto.donation;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetCharityDonorsOfTheMonthWrapperDto {
     private List<String> projectIds;
 }
