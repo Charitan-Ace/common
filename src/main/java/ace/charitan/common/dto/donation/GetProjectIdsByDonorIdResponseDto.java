8435a2c4aa713df00e96fe71a22dc4d6132397aa
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-12 14:35:07 +0700

    Add get project ids by donor id dto

---
+++ src/main/java/ace/charitan/common/dto/donation/GetProjectIdsByDonorIdResponseDto.java
@@ -0,0 +1,12 @@
+package ace.charitan.common.dto.donation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetProjectIdsByDonorIdResponseDto {
+    private GetProjectIdsByDonorIdWrapperDto wrapper;
+}
---
+++ src/main/java/ace/charitan/common/dto/donation/GetProjectIdsByDonorIdWrapperDto.java
@@ -0,0 +1,7 @@
+package ace.charitan.common.dto.donation;
+
+import java.util.List;
+
+public class GetProjectIdsByDonorIdWrapperDto {
+    private List<String> projectIds;
+}
