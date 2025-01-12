7b142a15f636e869703b447e85ab63c55c158a7e
LtMtrunG <s3927071@rmit.edu.vn>
2025-01-13 04:01:23 +0700

    dto filter project by status + dto to get new users

---
+++ src/main/java/ace/charitan/common/dto/auth/GetNewUserByTimeResponseDto.java
@@ -0,0 +1,4 @@
+package ace.charitan.common.dto.auth;
+
+public record GetNewUserByTimeResponseDto(UserIdListWrapDto userIdList) {
+}
---
+++ src/main/java/ace/charitan/common/dto/auth/UserIdListWrapDto.java
@@ -0,0 +1,7 @@
+package ace.charitan.common.dto.auth;
+
+import java.util.List;
+import java.util.UUID;
+
+public record UserIdListWrapDto(List<UUID> userIds) {
+}
