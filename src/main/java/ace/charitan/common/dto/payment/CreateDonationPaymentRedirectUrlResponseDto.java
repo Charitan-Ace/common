cfa8f71d040aac6c2190d817ec9ade821e3ff26f
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-13 00:08:09 +0700

    I love microservice

--- src/main/java/ace/charitan/common/dto/payment/CreateDonationPaymentRedirectUrlResponseDto.java
+++ src/main/java/ace/charitan/common/dto/payment/CreateDonationPaymentRedirectUrlResponseDto.java
@@ -1,5 +1,12 @@
 package ace.charitan.common.dto.payment;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateDonationPaymentRedirectUrlResponseDto {
     private String redirectUrl;
 }
