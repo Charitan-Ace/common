79bc9e3d4a54bd6af7f9d5c270344325008fb32f
phucdoan2003 <thienphucdoantran@gmail.com>
2025-01-12 23:55:54 +0700

    Added create donation payment redirect url

---
+++ src/main/java/ace/charitan/common/dto/payment/CreateDonationPaymentRedirectUrlRequestDto.java
@@ -0,0 +1,15 @@
+package ace.charitan.common.dto.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateDonationPaymentRedirectUrlRequestDto {
+    private Long donationId;
+    private Double amount;
+    private String successUrl;
+    private String cancelUrl;
+}
---
+++ src/main/java/ace/charitan/common/dto/payment/CreateDonationPaymentRedirectUrlResponseDto.java
@@ -0,0 +1,5 @@
+package ace.charitan.common.dto.payment;
+
+public class CreateDonationPaymentRedirectUrlResponseDto {
+    private String redirectUrl;
+}
