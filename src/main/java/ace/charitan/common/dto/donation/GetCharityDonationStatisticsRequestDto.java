2943462efc50568b3815dad343b17d1139fda24d
LtMtrunG <s3927071@rmit.edu.vn>
2025-01-13 23:32:35 +0700

    Add time to filter charity statistics

--- src/main/java/ace/charitan/common/dto/donation/GetCharityDonationStatisticsRequestDto.java
+++ src/main/java/ace/charitan/common/dto/donation/GetCharityDonationStatisticsRequestDto.java
@@ -8,5 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetCharityDonationStatisticsRequestDto {
-  private GetCharityDonationStatisticsWrapperDto dto;
+    private String time;
+    private GetCharityDonationStatisticsWrapperDto dto;
 }
